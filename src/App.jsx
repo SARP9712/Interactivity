be2ce0c39f51b7bfa905388a0bed3de482cb9a61
Samuel Rodriguez Paez <alonsopae1206@gmail.com>
2025-03-20 13:16:49 +0100

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Intro from './components/intro/intro';
+
+
+const App = () =>  {
+  const [isIntroFinished, setIntroFinished] = useState(false);
+
+  const handleIntroEnd = () => {
+    setIntroFinished(true); // Cuando la animación termine, mostramos el contenido
+  };
+
+  return (
+    <Router>
+    {!isIntroFinished && <Intro onIntroEnd={handleIntroEnd} />} {/* Muestra la intro si no ha terminado */}
+    {isIntroFinished && (
+      <>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </>
+    )}
+  </Router>
+  )
+}
+
+export default App
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { gsap } from "gsap/gsap-core"
+import './intro.css';
+
+const Intro = ({ onIntroEnd })=> {
+    useEffect( ()=> {
+        gsap.to('.intro-container',{
+            opacity:0,
+            duration:1,
+            delay:2,
+            onComplete:onIntroEnd,
+        })
+    }, [onIntroEnd])
+
+return(
+
+    <div className="intro-container">
+        <div className="intro-content">
+            <h1>Bienvenido a mi pagina</h1>
+            <p> Estamos cargando algo increible para uds</p>
+        </div>
+
+    </div>
+)
+
+}
+
+export default Intro
